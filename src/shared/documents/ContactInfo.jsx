00eca23a894ea683abc0fa944701099ea269f4bf
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-03-20 16:10:34 +0200

    sandy -> kandy

--- src/shared/documents/ContactInfo.jsx
+++ src/shared/documents/ContactInfo.jsx
@@ -5,7 +5,7 @@ export default function ContactInfo() {
         Email: <a href="mailto:stjohnshillestate55@gmail.com">stjohnshillestate55@gmail.com</a>
       </li>
       <li>Phone: 94777945486</li>
-      <li>St. Johns Hill Estate, Madulkelle, Sandy Sri Lanka</li>
+      <li>St. Johns Hill Estate, Madulkelle, Kandy Sri Lanka</li>
     </ul>
   )
 }
