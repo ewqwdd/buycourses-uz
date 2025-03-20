34332831480ad475567cdf59de0663553b1d80c2
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-03-20 12:25:41 +0200

    add cart text

--- src/widgets/Nav/CartLink.jsx
+++ src/widgets/Nav/CartLink.jsx
@@ -3,7 +3,8 @@ import Cart from '../../shared/icons/Cart.svg'
 
 export default function CartLink() {
   return (
-    <Link className={'size-[42px] flex justify-center items-center'} to={'/cart'}>
+    <Link className={'h-[42px] flex justify-center items-center mr-4 text-sm'} to={'/cart'}>
+      Cart
       <Cart className={'size-8 text-secondary'} />
     </Link>
   )
